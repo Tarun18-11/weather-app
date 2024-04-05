a4072d84fe25c5fc3d8c40dcb6da1c0ddee1ce99
TARUN <tarunkushwaha0006@gmail.com>
2024-04-05 20:18:55 +0530

    initial commit

--- weather.js
+++ weather.js
@@ -20,17 +20,17 @@ async function checkWeather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
     if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "/images/clear.png"
+      weatherIcon.src = "images/clear.png"
     } else if (data.weather[0].main == "Snow") {
-      weatherIcon.src = "/images/snow.png"
+      weatherIcon.src = "images/snow.png"
     } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "/images/rain.png"
+      weatherIcon.src = "images/rain.png"
     } else if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "/images/clouds.png"
+      weatherIcon.src = "images/clouds.png"
     } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "/images/mist.png"
+      weatherIcon.src = "images/mist.png"
     } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "/images/drizzle.png"
+      weatherIcon.src = "images/drizzle.png"
     }
 
     document.querySelector(".weather").style.display = "block";
